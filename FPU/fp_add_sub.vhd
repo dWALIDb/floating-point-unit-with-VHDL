32cabafbbda6c157d0cd6a067e5a76b51333d09e
dWALIDb <brazzawalidmokrane@gmail.com>
2024-09-10 11:54:38 +0100

    Update fp_add_sub.vhd

--- FPU/fp_add_sub.vhd
+++ FPU/fp_add_sub.vhd
@@ -8,8 +8,8 @@
 force -freeze sim:/fp_add_sub/clk 1 0, 0 {50 ps} -r 100
 force -freeze sim:/fp_add_sub/rst 1 0
 force -freeze sim:/fp_add_sub/go 1 0
-force -freeze sim:/fp_add_sub/A 01001000100000000000000000000000 0
-force -freeze sim:/fp_add_sub/B 01000000100000000000000000000000 0
+force -freeze sim:/fp_add_sub/A 11000000001000000000000000000000 0
+force -freeze sim:/fp_add_sub/B 01000000000000000000000000000000 0
 */
 library ieee;
 use ieee.std_logic_1164.all;
@@ -78,7 +78,8 @@ port (
 	Q:out std_logic_vector(data_width-1 downto 0)
 );
 end component;
-
+--some conditions to determine the larger or smaller values 
+signal equal_exponent,greater_exponent,equal_mantissa,greater_mantissa:std_logic;
 -- control signals 
 signal sign,mantissa_msb_for_subtraction,zero_exponent1,zero_exponent2,sub_mantissa,zero_mantissa,ld_larger,ld_smaller,ld_exponent,shift_mantissa,ld_mantissa,mantissa_msb: std_logic;
 signal shift_smaller,exponent_src:std_logic_vector(1 downto 0);
@@ -96,11 +97,21 @@ zero_exponent1<='1' when A(operand_width-2 downto operand_width-exponent_width-1
 
 zero_exponent2<='1' when B(operand_width-2 downto operand_width-exponent_width-1)=zero(exponent_width-1 downto 0) else '0';
 
+equal_exponent<='1' when A(operand_width-2 downto operand_width-exponent_width-1)=B(operand_width-2 downto operand_width-exponent_width-1) else '0';
+
+greater_exponent<='1' when A(operand_width-2 downto operand_width-exponent_width-1)>B(operand_width-2 downto operand_width-exponent_width-1) else '0';
+
+--equal_mantissa<='1' when A(mantissa_width-1 downto 0)=B(mantissa_width-1 downto 0) else '0';
+
+greater_mantissa<='1' when A(mantissa_width-1 downto 0)>B(mantissa_width-1 downto 0) else '0';
+
+
 exponent_difference<=std_logic_vector(unsigned(A(operand_width-2 downto operand_width-exponent_width-1))-unsigned(B(operand_width-2 downto operand_width-exponent_width-1)));
 
-sign<=A(operand_width-1) when A(operand_width-2 downto operand_width-exponent_width-1)>B(operand_width-2 downto operand_width-exponent_width-1) else B(operand_width-1);
+--either larger by exponent or equal exponent and larger mantissa
+sign<=A(operand_width-1) when (greater_exponent='1' or (greater_exponent='0' and equal_exponent='1' and greater_mantissa='1')) else B(operand_width-1);
 
-larger_exponent<=A(operand_width-2 downto operand_width-exponent_width-1) when A(operand_width-2 downto operand_width-exponent_width-1)>B(operand_width-2 downto operand_width-exponent_width-1) else B(operand_width-2 downto operand_width-exponent_width-1);
+larger_exponent<=A(operand_width-2 downto operand_width-exponent_width-1) when (greater_exponent='1' or (greater_exponent='0' and equal_exponent='1' and greater_mantissa='1')) else B(operand_width-2 downto operand_width-exponent_width-1);
 
 adjusted_exponent<=larger_exponent when exponent_src="00" else 
 				std_logic_vector(unsigned(registered_exponent) +1) when exponent_src="01" else
@@ -110,9 +121,9 @@ EXPONENT:generic_reg generic map(exponent_width) port map(clk,rst,ld_exponent,ad
 
 shift_amount<=exponent_difference when A(operand_width-2 downto operand_width-exponent_width-1)>B(operand_width-2 downto operand_width-exponent_width-1) else std_logic_vector(unsigned(not(exponent_difference))+1);
 
-larger<=A(mantissa_width-1 downto 0) when A(operand_width-2 downto operand_width-exponent_width-1)>B(operand_width-2 downto operand_width-exponent_width-1) else B(mantissa_width-1 downto 0);
+larger<=A(mantissa_width-1 downto 0) when (greater_exponent='1' or (greater_exponent='0' and equal_exponent='1' and greater_mantissa='1')) else B(mantissa_width-1 downto 0);
 
-smaller<=A(mantissa_width-1 downto 0) when A(operand_width-2 downto operand_width-exponent_width-1)<B(operand_width-2 downto operand_width-exponent_width-1) else B(mantissa_width-1 downto 0);
+smaller<=A(mantissa_width-1 downto 0) when not (greater_exponent='1' or (greater_exponent='0' and equal_exponent='1' and greater_mantissa='1')) else B(mantissa_width-1 downto 0);
 
 LARGER_OPERAND:generic_reg generic map(mantissa_width+exponent_width) port map(clk,rst,ld_larger,larger&zero(exponent_width-1 downto 0),A_mantissa); 
 
